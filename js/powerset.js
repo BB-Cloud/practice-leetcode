87843b0ce9ec05970c07942ffd1086b996e451bb
wangzixiang <3076693020@qq.com>
2019-11-28 09:33:52 +0800

    powerset

---
+++ js/powerset.js
@@ -0,0 +1,9 @@
+// Returns the powerset of a given array of numbers.
+
+// Use Array.prototype.reduce() combined with Array.prototype.map() to iterate over elements and combine into an array containing all combinations.
+
+const powerset = arr => arr.reduce((a, v) => a.concat(a.map(r => [v].concat(r))), [[]]);
+
+let a = powerset([1, 2]); // [[], [1], [2], [2, 1]]
+
+console.log(a);
