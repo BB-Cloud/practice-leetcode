e848b2055fd9fc2dac80d735d71c264e26a46833
wangzixiang <3076693020@qq.com>
2020-03-18 09:18:30 +0800

    function.prototype.call()

---
+++ call&apply&bind/fuc-apply.js
@@ -0,0 +1,40 @@
+// Function.prototype.apply()
+// 语法： func.apply(thisArg, [argsArray])
+// thisArg
+// 必选的。在 func 函数运行时使用的 this 值。请注意，this可能不是该方法看到的实际值：如果这个函数处于非严格模式下，则指定为 null 或 undefined 时会自动替换为指向全局对象，原始值会被包装。
+// argsArray
+// 可选的。一个数组或者类数组对象，其中的数组元素将作为单独的参数传给 func 函数。如果该参数的值为 null 或  undefined，则表示不需要传入任何参数。从ECMAScript 5 开始可以使用类数组对象。 浏览器兼容性 请参阅本文底部内容。
+
+/**
+ * 用 apply 将数组添加到另一个数组
+ * 
+ * **/
+var array = ['a', 'b'];
+var elements = [0, 1, 2];
+array.push.apply(array, elements);
+console.info(array); // ["a", "b", 0, 1, 2]
+
+/**
+ * 使用apply和内置函数
+ * 
+ * **/
+
+/* 找出数组中最大/小的数字 */
+var numbers = [5, 6, 2, 3, 7];
+
+/* 应用(apply) Math.min/Math.max 内置函数完成 */
+var max = Math.max.apply(this, numbers); /* 基本等同于 Math.max(numbers[0], ...) 或 Math.max(5, 6, ..) */
+var min = Math.min.apply(null, numbers);
+
+console.log(max, min);
+
+/* 代码对比： 用简单循环完成 */
+max = -Infinity;
+min = +Infinity;
+
+for (var i = 0; i < numbers.length; i++) {
+  if (numbers[i] > max)
+    max = numbers[i];
+  if (numbers[i] < min) 
+    min = numbers[i];
+}
